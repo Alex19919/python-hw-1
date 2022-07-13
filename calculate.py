64972cf3d38c639935f7ed57bff2e5c94c936f8f
Oleksandr <96943240+Alex19919@users.noreply.github.com>
2022-07-13 09:59:57 +0300

    new file

---
+++ calculate.py
@@ -0,0 +1,7 @@
+a = int(input('First number: '))
+b = int(input('Second number: '))
+
+d = int(input('Choose an arithmetic operation: \n 1 addition \n 2 subtraction \n 3 multiplication \n 4 division \n'))
+
+if d == 1:
+    r = a+b
